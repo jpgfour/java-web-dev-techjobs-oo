c9fdf513584be695792f561e1ec4e196bb857a61
jpgfour <jeremy.grise@gmail.com>
2020-05-12 17:15:12 -0500

    LC101 Unit 2 Assignment 2 - done through Step 4

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,47 @@
+package org.launchcode.techjobs_oo.Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.*;
+
+import static org.junit.Assert.*;
+
+public class JobTest {
+
+    Job testJob1;
+    Job testJob2;
+    Job testJob3;
+    Job testJob4;
+    Job testJob5;
+
+    @Before
+    public void createJobObjects(){
+        testJob1 = new Job();
+        testJob2 = new Job();
+        testJob3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        testJob4 = new Job("Regulatory Publisher", new Employer("MNK"), new Location("Hazelwood"), new PositionType("Reg Ops"), new CoreCompetency("deadline-driven"));
+        testJob5 = new Job("Regulatory Publisher", new Employer("MNK"), new Location("Hazelwood"), new PositionType("Reg Ops"), new CoreCompetency("deadline-driven"));
+    }
+
+    @Test
+    public void testSettingJobID(){
+        assertFalse(testJob1 == testJob2);
+
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        assertTrue(testJob3.getName() == "Product tester");
+        assertTrue(testJob3.getEmployer().getValue() == "ACME");
+        assertTrue(testJob3.getLocation().getValue() == "Desert");
+        assertTrue(testJob3.getPositionType().getValue() == "Quality control");
+        assertTrue(testJob3.getCoreCompetency().getValue() == "Persistence");
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        assertFalse(testJob4.equals(testJob5));
+        assertFalse(testJob5.equals(testJob4));
+    }
+
+}
